7b7f20f6ee68e0ceb9569e07cb2aa599f3fd0c32
henocmusau <henocmusau@gmail.com>
2023-04-29 01:40:30 +0200

    Corretion erreur casse ArticleItem

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import Header from './Header'
 import Hero from './Hero'
 import Navbar from './Navbar'
-import ArticleItem from './articleItem'
+import ArticleItem from './ArticleItem'
 
 export {
     Navbar,
